8a4c2ca8f39d0e909bc92a6bb1485180754797dc
Guilherme Alves <guialvest@gmail.com>
2022-07-21 15:12:51 -0300

    feat: setar banco de testes automáticamente com arquivo '.env.test' e execução de seed

---
+++ prisma/seed.ts
@@ -0,0 +1,12 @@
+import db from "../src/config/database.js";
+
+async function main() {}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
